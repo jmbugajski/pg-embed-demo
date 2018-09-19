a975b326988cf88fda31455795cca04f7341c4a9
jmbugajski <39573387+jmbugajski@users.noreply.github.com>
2018-09-19 12:41:47 -0500

    remove unused

--- tradepanel/js/get-selected.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() { 
-	$('#btn-ser1').click(function () { 
-		 var encoded = $("form#form-products").serialize();
-	$('div#form-output').text('Vals: '+ encoded); 
-		return false; 
-	});
-});
