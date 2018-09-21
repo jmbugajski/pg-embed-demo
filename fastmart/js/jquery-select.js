22516db31cb88063133f3bef84c5da859ff9dddd
jmbugajski <39573387+jmbugajski@users.noreply.github.com>
2018-09-20 20:48:05 -0700

    remove old code

--- fastmart/js/jquery-select.js
+++ fastmart/js/jquery-select.js
@@ -46,25 +46,4 @@ $(document).ready(function () {
 			$("#level").html("<option value='level-none'>-- Cluster --</option>");
 		}
 	});
-
-	$("#hier1").change(function () {
-		var val = $(this).val();
-		if (val == "hier1-bs") {
-			$("#hier2").html("<option value='hier2-camay'>Camay</option><option value='hier2-ivory'>Ivory</option><option value='hier2-olay'>Olay</option><option value='hier2-oldspice'>Old Spice</option><option value='hier2-safeguard'>Safeguard</option>");
-		} else if (val == "hier1-bt") {
-			$("#hier2").html("<option value='hier2-charmin'>Charmin</option>");
-		} else if (val == "hier1-bw") {
-			$("#hier2").html("<option value='hier2-gilette'>Gilette</option><option value='hier2-herbalessences'>Herbal Essences</option><option value='hier2-ivory'>Ivory</option><option value='hier2-olay'>Olay</option><option value='hier2-oldspice'>Old Spice</option>");
-		} else if (val == "hier1-ft") {
-			$("#hier2").html("<option value='hier2-puffs'>Puffs</option>");
-		} else if (val == "hier1-hl") {
-			$("#hier2").html("<option value='hier2-ivory'>Ivory</option>");
-		} else if (val == "hier1-np") {
-			$("#hier2").html("<option value='hier2-bounty'>Bounty</option>");
-		} else if (val == "hier1-pt") {
-			$("#hier2").html("<option value='hier2-bounty'>Bounty</option>");
-		} else if (val == "hier1-none") {
-			$("#hier2").html("<option value='hier2-none'>-- Brand --</option>");
-		}
-	});
 });
