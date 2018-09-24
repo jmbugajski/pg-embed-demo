daf21f3f172a6269b2a7a230dd218bdcd05fe93f
jmbugajski <39573387+jmbugajski@users.noreply.github.com>
2018-09-24 10:50:30 -0700

    add labels for store selectors

--- fastmart/js/jquery-select.js
+++ fastmart/js/jquery-select.js
@@ -11,24 +11,6 @@ $(document).on('change', '#store', function(e) {
 		$("#overview").html("<a target='iframe_a' href='https://35.227.249.152/embed/dashboards/13'>Overview</a>");
 	}
 });
-/*
-						<label for="sel-sector"><input type="checkbox" name="sel-sector" value="1"/>Sector</label>
-
-						<div id="checkboxes3">
-							<label for="sec1-bc"><input type="checkbox" name="sec1-bc" value="1"/>BC</label>
-							<label for="sec1-hhc"><input type="checkbox" name="sec1-hhc" value="1"/>HHC</label>
-						</div>
-
-						<div id="checkboxes4">
-							<label for="hier1-bs"><input type="checkbox" name="hier1-bs" value="1"/>Bar Soap</label>
-							<label for="hier1-bt"><input type="checkbox" name="hier1-bt" value="1"/>Bath Tissue</label>
-							<label for="hier1-bw"><input type="checkbox" name="hier1-bw" value="1"/>Body Wash</label>
-							<label for="hier1-ft"><input type="checkbox" name="hier1-ft" value="1"/>Facial Tissue</label>
-							<label for="hier1-hl"><input type="checkbox" name="hier1-hl" value="1"/>Hand Liquids</label>
-							<label for="hier1-np"><input type="checkbox" name="hier1-np" value="1"/>Napkins</label>
-							<label for="hier1-pt"><input type="checkbox" name="hier1-pt" value="1"/>Paper Towels</label>
-						</div>
-*/
 
 $(document).ready(function () {
 	$('[name="sel-sector"]').change(function (e) {
@@ -40,11 +22,6 @@ $(document).ready(function () {
 	$('[name="sel-brand"]').change(function (e) {
 		$('#select-hier2').toggle(this.checked);
 	});
-/*	
-	$('[name="sec1-bc"]').change(function (e) {
-		$('[name="hier1-bs"]').toggle(this.checked);
-	});
-*/
 });
 
 $(document).ready(function () {
